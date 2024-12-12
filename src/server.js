22f67b51cd2cb50258962d266a472e4c428fdadd
Charles Caleb <charlesofficial9@gmail.com>
2024-12-12 17:37:03 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
 const connectDB = require("./config/db.js");
 const authRoutes = require("./routes/auth.js");
 const diaryRoutes = require("./routes/dairy.js");
+const swaggerDocument = require("../swagger.json");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
@@ -15,10 +16,15 @@ app.use(cors());
 app.use(express.json());
 
 // Swagger Documentation
+app.use(
+  "/cohort3-fullstack-dairy/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
 // Routes
 app.use("/api/cohort3-fullstack/auth", authRoutes);
-app.use("/api/cohort3-fullstack/diary", diaryRoutes);
+app.use("/api/cohort3-fullstack/dairy", diaryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
