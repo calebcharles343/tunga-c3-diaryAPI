bb72f961fb80696716ed307fddcb583f28da1d61
Charles Caleb <charlesofficial9@gmail.com>
2024-12-12 18:46:00 +0100

    update

--- src/routes/dairy.js
+++ src/routes/dairy.js
@@ -11,10 +11,10 @@ const protect = require("../middleware/protect.js");
 
 const router = express.Router();
 
-router.get("/:id", protect, getAllEntries);
-router.get("/:id", protect, getEntry);
-router.post("/:id", protect, createEntry);
-router.put("/:id", protect, updateEntry);
-router.delete("/:id", protect, deleteEntry);
+router.get("/entries/:userId", protect, getAllEntries);
+router.get("/entry/:entryId", protect, getEntry);
+router.post("create/:userId", protect, createEntry);
+router.put("update/:entryId", protect, updateEntry);
+router.delete("delete/:entryId", protect, deleteEntry);
 
 module.exports = router;
